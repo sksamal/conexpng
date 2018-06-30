68a664f666a914508c87bafa59bfeb5c6243392f
Suraj Ketan Samal <sksamal@gmail.com>
2018-06-29 21:02:21 -0500

    Fri Jun 29 21:02:21 CDT 2018

---
+++ ingExtract.py
@@ -0,0 +1,18 @@
+import json
+import sys
+
+#for i in range(1,20):
+f = open(sys.argv[1],"r")
+values = json.load(f)
+f.close()
+
+for criteria in values['hits']:
+   label=criteria['recipe']['label']
+   image=criteria['recipe']['image']
+   source=criteria['recipe']['source']
+   ingredients=""
+   for value in criteria['recipe']['ingredients']:
+       print(value['text'])
+#       ingredients= ingredients + "$" + (value['text'] + "$" + str(value['weight']))
+#   print(label + "$" + image + "$" + source + "$" + ingredients)
+#   print(label + "$" + image + "$" + source + "$" + ingredients)
